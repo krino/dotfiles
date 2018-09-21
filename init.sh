e37e116a9efcd78fe62df8e52d0f53f0520d4b38
Giovanni Nappi <giovanni.nappi@truecaller.com>
2018-09-21 10:27:55 +0200

    Integration with oh-my-zsh

--- init.sh
+++ init.sh
@@ -11,13 +11,17 @@ curl https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh -o ~
 echo "git-completion and git-prompt Installed and Configured" >> $log_file
 
 # install zsh
-sudo apt-get -y install zsh 
+sudo apt-get -y install zsh curl
 if type -p zsh > /dev/null; then
     echo "zsh Installed" >> $log_file
 else
     echo "zsh FAILED TO INSTALL!!!" >> $log_file
 fi
 sudo apt-get install zsh-syntax-highlighting xcape
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+ln -s dotfiles/oh-my-zsh-custom.zsh ~/.oh-my-zsh/
 
 # install tmux
 sudo apt-get -y install tmux tmuxinator
---
+++ oh-my-zsh-custom.zsh
@@ -0,0 +1,29 @@
+# Auto CD
+setopt auto_cd
+
+# Remap Caps Lock with Ctrl
+# use gnome tweaks
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/giovanni/Downloads/google-cloud-sdk-198.0.0-linux-x86_64/google-cloud-sdk/path.zsh.inc' ]; then 
+  source '/home/giovanni/Downloads/google-cloud-sdk-198.0.0-linux-x86_64/google-cloud-sdk/path.zsh.inc'; 
+fi
+
+if [ $commands[kubectl] ]; then
+  source <(kubectl completion zsh)
+fi
+
+export GOPATH=~/work/go
+export GOBIN=$GOPATH/bin
+export PATH=$GOBIN:$PATH
+export EDITOR=vim
+
+# alias vim with nvim
+alias vim=$(which nvim)
+alias vi=$(which nvim)
+alias v=$(which nvim)
+
+alias t=$(which tmuxinator)
+
+# set PATH so it includes user's private bin directories
+PATH="$HOME/bin:$HOME/.local/bin:$PATH"
